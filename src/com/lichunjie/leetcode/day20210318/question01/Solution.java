97f0a1d5606dbe945f3951e038d80f4dd6e189e5
lichunjie <admin@lichunjie.com>
2021-03-18 21:00:38 +0800

    提交题解:删除排序数组中的重复项

---
+++ src/com/lichunjie/leetcode/day20210318/question01/Solution.java
@@ -0,0 +1,25 @@
+package com.lichunjie.leetcode.day20210318.question01;
+
+public class Solution {
+    public static int removeDuplicates(int[] nums) {
+        if (null == nums || 0 == nums.length) return 0;
+        int index = 0;
+        int length = nums.length;
+        for (int j = 1;j<length;j++){
+            if (nums[index] != nums[j]){
+                index++;
+                nums[index]=nums[j];
+            }
+        }
+        return index+1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 0, 0, 1, 1, 2, 2, 3, 3, 4};
+        int result = removeDuplicates(arr);
+        System.out.println("数组大小为:"+result);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
